daaf089859a188bbf4943ab41b5ca16d6bc45a4a
Joey <joey.chik05@gmail.com>
2025-05-29 22:45:51 +0800

    feat: add contribute and get started buttons

--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import NavBar from "../../components/NavBar";
 
 export default function LandingPage() {
@@ -5,10 +6,23 @@ export default function LandingPage() {
     <>
       <div className="min-h-screen flex flex-col">
         <NavBar />
-        <main className="flex-grow">
+        <main className="flex-grow space-y-8">
           <h1 className="text-2xl font-bold text-center mt-6">
             Welcome to Where Got Time
           </h1>
+          <div className="flex flex-row justify-center space-x-10">
+            <Link to="/signup" className="text-indigo-800">
+              <button className="bg-indigo-100 rounded-3xl px-4 py-2 hover:underline font-semibold">
+                Get Started!
+              </button>
+            </Link>
+
+            <Link to="/contribute" className="text-indigo-100">
+              <button className="bg-indigo-800 rounded-3xl px-4 py-2 hover:underline font-semibold">
+                Contribute
+              </button>
+            </Link>
+          </div>
         </main>
       </div>
     </>
--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/authentication/login/LoginPage.tsx";
 import SignupPage from "../pages/authentication/signup/SignupPage.tsx";
 import LandingPage from "../pages/landing/LandingPage.tsx";
+import ContributePage from "../pages/landing/ContributePage.tsx";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/contribute" element={<ContributePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
