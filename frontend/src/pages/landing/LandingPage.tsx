374527dd9c2cb9bef27545ea3e9ae93541829b66
Joey <joey.chik05@gmail.com>
2025-05-29 20:07:41 +0800

    refactor: update routing logic

--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -1,11 +1,16 @@
 import NavBar from "../../components/NavBar";
+
 export default function LandingPage() {
   return (
     <>
-      <NavBar />
-      <h1 className="text-2xl font-bold text-center mt-6">
-        Welcome to Where Got Time
-      </h1>
+      <div className="min-h-screen flex flex-col">
+        <NavBar />
+        <main className="flex-grow">
+          <h1 className="text-2xl font-bold text-center mt-6">
+            Welcome to Where Got Time
+          </h1>
+        </main>
+      </div>
     </>
   );
 }
--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,15 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-// import files used for the routing (nav bar)
-import NavBar from "../components/NavBar.tsx";
+// import files used for the routing (landing page for now)
 import LoginPage from "../pages/authentication/login/LoginPage.tsx";
 import SignupPage from "../pages/authentication/signup/SignupPage.tsx";
+import LandingPage from "../pages/landing/LandingPage.tsx";
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<NavBar />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
       </Routes>
