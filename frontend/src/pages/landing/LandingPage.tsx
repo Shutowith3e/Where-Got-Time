ffadce548f0a2d4dbb4df836e840fa0842456ef2
Joey <joey.chik05@gmail.com>
2025-05-29 23:05:44 +0800

    chore: update landing page details

--- frontend/src/pages/landing/LandingPage.tsx
+++ frontend/src/pages/landing/LandingPage.tsx
@@ -6,10 +6,16 @@ export default function LandingPage() {
     <>
       <div className="min-h-screen flex flex-col">
         <NavBar />
-        <main className="flex-grow space-y-8">
-          <h1 className="text-2xl font-bold text-center mt-6">
-            Welcome to Where Got Time
+        <main className="flex-grow space-y-12">
+          <h1 className="text-4xl font-extrabold text-center mt-6">
+            Effortless Group Scheduling For Everyone
           </h1>
+          <h2 className="text-center mt-6 font-stretch-75% text-2xl text-slate-800">
+            Say goodbye to scheduling chaos. Combine calendars, detect clashes,
+            and <br />
+            stay synced with your groups — whether for lessons, CCAs, or project
+            meetings.
+          </h2>
           <div className="flex flex-row justify-center space-x-10">
             <Link to="/signup" className="text-indigo-800">
               <button className="bg-indigo-100 rounded-3xl px-4 py-2 hover:underline font-semibold">
