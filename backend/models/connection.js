0e539634769cd01fe67e9a7813ee5681d0ab37e8
Lim-JL <limjiale.ljl@gmail.com>
2025-05-31 02:58:26 +0800

    bug fix and overhaul of all require statements
    javascript is a joke language

--- backend/models/connection.js
+++ backend/models/connection.js
@@ -1,8 +1,11 @@
-const { createClient } = require("@supabase/supabase-js");
+import {createClient} from "@supabase/supabase-js";
+import env from "dotenv";
+env.config();
 const supabase = createClient(
   process.env.SUPABASE_URL,
   process.env.SUPABASE_KEY
 );
 
-module.exports = supabase;
+// module.exports = supabase;
+export default supabase;
 // shld be all
--- backend/models/taskService.js
+++ backend/models/taskService.js
@@ -1,5 +1,5 @@
-const supabase = require("./connection");
+import supabase from "./connection";
 // make functions to query db here
 //eg const get users = async () => {return await supabase.from("user").select("*")}
 
-//module.exports = {all your functions}
+//export {func1,func2, etc}
