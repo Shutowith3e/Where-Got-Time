170677677acf3d51b081088d9564dd49d321a65b
Lim-JL <limjiale.ljl@gmail.com>
2025-05-31 18:12:05 +0800

    updated structure for routing of each entity

--- backend/models/eventService.js
+++ backend/models/eventService.js
@@ -1,5 +1,6 @@
 import supabase from "./connection";
 // make functions to query db here
+//also add functions to handle other business rules here
 //eg const get users = async () => {return await supabase.from("user").select("*")}
 
 //export {func1,func2, etc}
