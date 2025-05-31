97a9ea364377bb601a9ef489641204a8cc871e46
Joey <joey.chik05@gmail.com>
2025-06-01 00:02:56 +0800

    style: change colour of reason card

--- frontend/src/pages/landing/ReasonCard.tsx
+++ frontend/src/pages/landing/ReasonCard.tsx
@@ -5,11 +5,11 @@ export type ReasonCardProps = {
 
 export default function ReasonCard({ title, reason }: ReasonCardProps) {
   return (
-    <div className="flex flex-col flex-wrap bg-rose-900/80 space-y-6 space-x-4 text-center rounded-3xl justify-center items-center">
-      <h3 className="text-xl font-semibold text-rose-50 pl-8 pr-8 pt-4">
+    <div className="flex flex-col flex-wrap bg-gradient-to-b from-rose-950/66 to-rose-900/70 space-y-6 space-x-4 text-center rounded-3xl justify-center items-center">
+      <h3 className="text-xl font-semibold text-rose-50 pl-10 pr-10 pt-6">
         {title}
       </h3>
-      <p className="text-lg text-rose-100 pl-8 pr-8 pb-8">{reason}</p>
+      <p className="text-lg text-rose-100 pl-10 pr-10 pb-8">{reason}</p>
     </div>
   );
 }
