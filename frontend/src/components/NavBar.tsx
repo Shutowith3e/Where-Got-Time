d91cdeb8c6b8ec3eb3334a72b3aa3dda539027e0
Joey <joey.chik05@gmail.com>
2025-06-01 13:30:24 +0800

    bug: fix nested buttons

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -8,14 +8,15 @@ export default function NavBar() {
           <img src="/logo.png" className="max-h-10"></img>
         </div>
 
-        <div className="flex justify-end items-center gap-6">
+        <div className="flex justify-end items-center gap-6 ">
           <Link to="/signup" className="text-indigo-800 hover:underline">
             Sign Up
           </Link>
-          <Link to="/login" className="text-indigo-800">
-            <button className="bg-indigo-300 rounded-3xl px-4 py-2 hover:underline font-semibold">
-              Log In
-            </button>
+          <Link
+            to="/login"
+            className="text-indigo-800 bg-indigo-300 rounded-3xl px-4 py-2 hover:underline font-semibold"
+          >
+            Log In
           </Link>
         </div>
       </nav>
