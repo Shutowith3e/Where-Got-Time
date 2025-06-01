ce74302791ff9e06f6dea6183ceac6d337a288c6
Joey <joey.chik05@gmail.com>
2025-06-01 13:05:55 +0800

    refactor: fix padding for card

--- frontend/src/pages/landing/ReasonCard.tsx
+++ frontend/src/pages/landing/ReasonCard.tsx
@@ -6,10 +6,10 @@ export type ReasonCardProps = {
 export default function ReasonCard({ title, reason }: ReasonCardProps) {
   return (
     <div className="flex flex-col flex-wrap bg-gradient-to-b from-rose-950/66 to-rose-900/70 space-y-6 space-x-4 text-center rounded-3xl justify-center items-center">
-      <h3 className="text-xl font-semibold text-rose-50 pl-10 pr-10 pt-6">
+      <h3 className="text-xl font-semibold text-rose-50 mx-auto pt-6">
         {title}
       </h3>
-      <p className="text-lg text-rose-100 pl-10 pr-10 pb-8">{reason}</p>
+      <p className="text-lg text-rose-100 mx-auto my-3 pb-8">{reason}</p>
     </div>
   );
 }
