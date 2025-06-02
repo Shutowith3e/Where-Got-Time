f1311aa81cc3832f1ecf6a504d575683cd1903ad
Lim-JL <limjiale.ljl@gmail.com>
2025-06-02 16:51:58 +0800

    bug fix: deleted extra files

--- backend/controllers/taskController.js
+++
@@ -1,3 +0,0 @@
-import * as service from "../models/taskService";
-//TODO implement functionalities for routes here
-// export {fun1,fun2,etc}
--- backend/routers/taskRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-const router = express.Router();
-//import {task1,task2,etc} from "../controllers/taskController";
-
-//router.get
-//router.post etc etc for each of the routes
-
-//export default router;
