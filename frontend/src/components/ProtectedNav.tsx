fedef64b40a94132f3b4da8c9cbf746004d530d5
Joey <joey.chik05@gmail.com>
2025-06-08 20:20:44 +0800

    feat: put logo into nav bar and update the css

--- frontend/src/components/ProtectedNav.tsx
+++ frontend/src/components/ProtectedNav.tsx
@@ -10,13 +10,17 @@ export default function ProtectedNavBar() {
     navigate("/login");
   };
   return (
-    <div className="flex justify-end">
+    <nav className="grid grid-cols-2 items-center px-8 py-4 p-4 bg-white">
+      <div className="flex items-center gap-3">
+        <img src="/logo.png" className="max-h-10"></img>
+      </div>
+
       <button
         onClick={signOut}
         className="text-indigo-800 bg-indigo-300 rounded-3xl px-4 py-2 hover:underline font-semibold"
       >
         Log Out
       </button>
-    </div>
+    </nav>
   );
 }
