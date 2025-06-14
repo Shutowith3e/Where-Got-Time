3130ce3fb364936f844974d9e187f2e6f101e534
Joey <joey.chik05@gmail.com>
2025-06-14 14:06:24 +0800

    feat: sort the events by timing and groups by name

--- frontend/src/components/EventCard.tsx
+++ frontend/src/components/EventCard.tsx
@@ -16,7 +16,7 @@ export type EventCardProps = {
 
 function EventChip({ event: { count, event, group, date } }: EventChipProps) {
   return (
-    <div className="flex flex-row bg-white m-auto rounded-2xl text-base font-semibold px-8 py-1 gap-x-4 mt-2 min-w-45">
+    <div className="flex flex-row bg-slate-100 m-auto rounded-2xl text-base font-semibold px-8 py-1 gap-x-4 mt-2 min-w-45">
       <div className="text-sm font-light m-auto">{date}</div>
       {group.toUpperCase()} - {event}
       <div className="text-sm font-light m-auto">({count} Members)</div>
@@ -26,11 +26,13 @@ function EventChip({ event: { count, event, group, date } }: EventChipProps) {
 
 export default function EventCard({ title, events }: EventCardProps) {
   return (
-    <div className="flex flex-col bg-violet-300/15 p-4 rounded-lg m-4">
-      <h3 className="text-xl mx-auto font-bold px-4 ">{title}</h3>
-      {events.map((x) => (
-        <EventChip event={x} />
-      ))}
+    <div className="flex flex-col bg-white p-4 rounded-xl m-4 gap-y-0.5 drop-shadow-xl drop-shadow-rose-800/8 ">
+      <h3 className="text-xl mx-auto font-bold px-4 mb-4">{title}</h3>
+      {events
+        .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime())
+        .map((x) => (
+          <EventChip event={x} />
+        ))}
     </div>
   );
 }
