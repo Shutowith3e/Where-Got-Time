fa4d444194f46fb07263ab0b6676e02f3dd27f6a
Joey <joey.chik05@gmail.com>
2025-06-15 14:13:01 +0800

    feat: calendar placeholder

---
+++ frontend/src/components/IndividualCalendar.tsx
@@ -0,0 +1,10 @@
+export default function IndividualCalendar() {
+  return (
+    <div className="mx-auto bg-gradient-to-b from-rose-900/30 min-h-[70vh] w-full text-center rounded-3xl">
+      <div className="p-2 ">
+        <button className="rounded-2xl bg-orange-200 p-1 px-8">Week</button>
+        <p className="m-auto text-2xl">CALENDAR PLACEHOLDER</p>
+      </div>
+    </div>
+  );
+}
