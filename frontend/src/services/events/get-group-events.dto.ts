06e6d1b0d08834767b1792af8f7c2fed7a4107ee
Joey <joey.chik05@gmail.com>
2025-06-24 18:51:32 +0800

    feat: get group events from be

---
+++ frontend/src/services/events/get-group-events.dto.ts
@@ -0,0 +1,27 @@
+// {
+//     "data": [
+//         {
+//             "eid": "11c5101c-a31f-45c9-ae90-548ac3558888",
+//             "gid": "c5a45461-b148-4d04-941a-c382669b93e2",
+//             "event_name": "Testing event",
+//             "start_datetime": "2025-06-23T18:24:03",
+//             "end_datetime": "2025-06-30T18:24:10",
+//             "high_priority": true,
+//             "rrule": null
+//         }
+//     ]
+// }
+
+type GroupEventData = {
+  eid: string;
+  gid: string;
+  eventName: string;
+  startDatetime: string;
+  endDatetime: string;
+  highPriority: boolean;
+  rrule: null | string;
+};
+
+export type GetGroupEventDto = {
+  data: GroupEventData[];
+};
