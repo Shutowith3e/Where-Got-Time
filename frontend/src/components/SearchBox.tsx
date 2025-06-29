bf7c870dee16ea710bc746e692bc08e928ea883f
Joey <joey.chik05@gmail.com>
2025-06-29 18:28:38 +0800

    fix: css and the centering of emails

--- frontend/src/components/SearchBox.tsx
+++ frontend/src/components/SearchBox.tsx
@@ -9,8 +9,8 @@ function MemberSearch({
 }) {
   return (
     <p
-      className="w-58 overflow-ellipsis overflow-hidden whitespace-nowrap font-light cursor-pointer
-hover:bg-slate-100"
+      className="w-56 mx-auto overflow-ellipsis overflow-hidden whitespace-nowrap font-light cursor-pointer
+hover:bg-slate-100 "
       onClick={onClick}
     >
       {email}
@@ -31,23 +31,23 @@ export default function SearchBox({
   setSelectedEmails,
 }: EmailResultProps) {
   return (
-    <div className="flex absolute bg-slate-50 w-full border border-slate-200 rounded-b-lg z-index-20">
-      <div className="p-1 flex-col">
-        {(emails ?? []).map((email) => (
-          <MemberSearch
-            key={email}
-            email={email}
-            onClick={() => {
-              if (!selectedEmails.includes(email)) {
-                setSelectedEmails((prev_arr) => {
-                  return prev_arr.includes(email)
-                    ? prev_arr
-                    : [...prev_arr, email];
-                });
-              }
-            }}
-          />
-        ))}
+    <div className="relative w-full">
+      <div className="flex absolute bg-slate-50 w-full overflow-auto border border-slate-200 rounded-b-lg z-index-20">
+        <div className="p-1 flex flex-col items-center w-full">
+          {(emails ?? []).map((email) => (
+            <MemberSearch
+              key={email}
+              email={email}
+              onClick={() => {
+                if (!selectedEmails.includes(email)) {
+                  setSelectedEmails((prev_arr) =>
+                    prev_arr.includes(email) ? prev_arr : [...prev_arr, email]
+                  );
+                }
+              }}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
