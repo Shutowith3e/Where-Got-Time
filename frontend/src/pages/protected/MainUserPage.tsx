459ff70d298e0e70a43908b8c7a9c398ec4f3b86
Joey <joey.chik05@gmail.com>
2025-07-08 19:25:30 +0800

    fix: create event

--- frontend/src/pages/protected/MainUserPage.tsx
+++ frontend/src/pages/protected/MainUserPage.tsx
@@ -8,11 +8,11 @@ import {
 } from "@/components/ui/sidebar/sidebar";
 import dayjs from "dayjs";
 import { useQuery } from "@tanstack/react-query";
-import {
-  getUserEventsInRange,
-} from "@/services/events/get-user-events-data";
+import { getUserEventsInRange } from "@/services/events/get-user-events-data";
 import EventCard from "@/components/ui/event/EventCard";
 import useAuth from "@/context/AuthContext";
+import { GroupContextProvider } from "@/context/GroupContext";
+import { getGroupInfo } from "@/services/groups/get-group-info";
 
 export default function MainUserPage() {
   const [showCreateModal, setShowCreateModal] = useState(false);
@@ -26,81 +26,90 @@ export default function MainUserPage() {
       ),
   });
 
+  const { data: group, isPending: isGroupsPending } = useQuery({
+    queryKey: ["user-group", personalGroupId],
+    queryFn: () => getGroupInfo(personalGroupId!),
+  });
+
+  if (!group) return <p>Loading...</p>;
+
   return (
-    <div className="flex min-h-dvh flex-col bg-gradient-to-b from-rose-900/30 ">
-      <NavBar />
+    <GroupContextProvider groupInfo={group}>
+      <div className="flex min-h-dvh flex-col bg-gradient-to-b from-rose-900/30 ">
+        <NavBar />
 
-      <div className="flex flex-1 flex-row">
-        <SidebarProvider defaultOpen={false}>
-          <MainUserDrawer />
+        <div className="flex flex-1 flex-row">
+          <SidebarProvider defaultOpen={false}>
+            <MainUserDrawer />
 
-          <div className="mt-2 ml-1">
-            <SidebarTrigger />
-          </div>
-
-          <div className="flex-1 px-3 py-6">
-            {/* Week Navigation */}
-            <div className="mb-5 flex items-center justify-center gap-5">
-              <button className="rounded bg-gray-400 px-4 py-2 text-white">
-                ◀
-              </button>
-              <span className="font-semibold">Week</span>
-              <button className="rounded bg-gray-400 px-4 py-2 text-white">
-                ▶
-              </button>
+            <div className="mt-2 ml-1">
+              <SidebarTrigger />
             </div>
 
-            {/* Calendar Placeholder */}
-            <div className="mb-6 h-96 w-full rounded-lg bg-white shadow">
-              <p className="flex h-full items-center justify-center font-bold text-gray-400">
-                CALENDAR (FULL HEIGHT)
-              </p>
-            </div>
+            <div className="flex-1 px-3 py-6">
+              {/* Week Navigation */}
+              <div className="mb-5 flex items-center justify-center gap-5">
+                <button className="rounded bg-gray-400 px-4 py-2 text-white">
+                  ◀
+                </button>
+                <span className="font-semibold">Week</span>
+                <button className="rounded bg-gray-400 px-4 py-2 text-white">
+                  ▶
+                </button>
+              </div>
 
-            {/* Your Events */}
-            <div className="rounded-lg bg-white p-6">
-              <div className="mb-4 flex flex-wrap items-center justify-between">
-                <h2 className="text-lg font-semibold">Personal Group</h2>
+              {/* Calendar Placeholder */}
+              <div className="mb-6 h-96 w-full rounded-lg bg-white shadow">
+                <p className="flex h-full items-center justify-center font-bold text-gray-400">
+                  CALENDAR (FULL HEIGHT)
+                </p>
+              </div>
 
-                <div className="flex gap-3">
-                  {/* Create Event Button */}
-                  <button
-                    onClick={() => setShowCreateModal(true)}
-                    className="rounded bg-green-600 px-4 py-2 text-white hover:bg-green-700"
-                  >
-                    Create Event
-                  </button>
-                  {showCreateModal && (
-                    <CreateEventModal
-                      onClose={() => setShowCreateModal(false)}
-                      gid={""}
-                    />
-                  )}
+              {/* Your Events */}
+              <div className="rounded-lg bg-white p-6">
+                <div className="mb-4 flex flex-wrap items-center justify-between">
+                  <h2 className="text-lg font-semibold">Personal Group</h2>
+
+                  <div className="flex gap-3">
+                    {/* Create Event Button */}
+                    <button
+                      onClick={() => setShowCreateModal(true)}
+                      className="rounded bg-green-600 px-4 py-2 text-white hover:bg-green-700"
+                    >
+                      Create Event
+                    </button>
+                    {showCreateModal && (
+                      <CreateEventModal
+                        onClose={() => setShowCreateModal(false)}
+                        gid={personalGroupId}
+                      />
+                    )}
+                  </div>
+                </div>
+
+                <div className="rounded-xl bg-purple-100 px-4 py-2 text-sm shadow-inner">
+                  <EventCard
+                    title={"Upcoming Events"}
+                    events={(groupEventList ?? []).map(
+                      ({
+                        groupName,
+                        eventName,
+                        startDatetime,
+                        highPriority,
+                      }) => ({
+                        group: groupName,
+                        eventName: eventName,
+                        date: dayjs(startDatetime).format("DD MMM (hh:m A)"),
+                        highPriority,
+                      })
+                    )}
+                  ></EventCard>
                 </div>
               </div>
-
-              <div className="rounded-xl bg-purple-100 px-4 py-2 text-sm shadow-inner">
-                <EventCard
-                  title={"Upcoming Events"}
-                  events={(groupEventList ?? []).map(
-                    ({
-                      groupName,
-                      eventName,
-                      startDatetime,
-                      highPriority,
-                    }) => ({
-                      group: groupName,
-                      eventName: eventName,
-                      date: dayjs(startDatetime).format("DD MMM (hh:m A)"),
-                      highPriority,
-                    })
-                  )}
-                ></EventCard>
-              </div>
             </div>
-          </div>
-        </SidebarProvider>
+          </SidebarProvider>
+        </div>
       </div>
-    </div>
+    </GroupContextProvider>
   );
 }
