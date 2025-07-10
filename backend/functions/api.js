b9e968eba5fd2baf8ecf944abf2533882d17a717
Joey <joey.chik05@gmail.com>
2025-07-10 14:02:05 +0800

    ci: restructure file

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -2,6 +2,13 @@ import express, { Router } from 'express';
 import cors from "cors";
 import env from "dotenv";
 import serverless from "serverless-http";
+import authJWT from '../middleware/auth.js';
+import mCheckAdmin from '../middleware/checkAdminMiddleware.js';
+import groupRoutes from '../routers/groupRouter.js';
+import eventRoutes from '../routers/eventRouter.js';
+import userRoutes from '../routers/userRouter.js';
+import adminRoutes from '../routers/adminRouter.js';
+
 
 env.config();
 
@@ -11,44 +18,27 @@ const app = express();
 // for parsing JSON bodies
 app.use(express.json());
 
-// this allows all origins, replace it before deployment
-app.use(cors());
-app.options("*", cors());
-
-// replace it with this! 
-// app.use(cors({
-//   origin: "http://localhost:3000", 
-//   credentials: true
-// }));
-
-
 // for parsing URL-encoded form data
 app.use(express.urlencoded({ extended: true }));
 
+// this allows all origins
+app.use(cors());
+app.options("*", cors());
+
+
 // import middleware for auth
-import authJWT from '../middleware/auth.js';
 app.use(authJWT); // all routes after this needs a jwt in the auth header to work
 
 // import and use exported routes from groupRouter 
 const allRoutes = Router();
-import groupRoutes from '../routers/groupRouter.js';
-import eventRoutes from '../routers/eventRouter.js';
-import userRoutes from '../routers/userRouter.js';
-import adminRoutes from '../routers/adminRouter.js';
 
 allRoutes.use('/groups', groupRoutes);
 allRoutes.use('/events', eventRoutes);
 allRoutes.use('/users', userRoutes);
-
-//auth test route, delete later
-import authrouter from '../authtest.js';
-allRoutes.use('/test', authrouter);
-
 //import middleware for admin verification
-import mCheckAdmin from '../middleware/checkAdminMiddleware.js';
-
 allRoutes.use('/admins', mCheckAdmin, adminRoutes)
-app.use("/api", allRoutes)
+
+app.use('/api', allRoutes)
 // check if server is running
 //app.listen(8000, () => console.log('Server running on port 8000'));
 export const handler = serverless(app, {
@@ -57,7 +47,6 @@ export const handler = serverless(app, {
       try {
         req.body = JSON.parse(event.body);
       } catch (e) {
-        // Leave it as is if it’s not JSON
       }
     }
     return req;
