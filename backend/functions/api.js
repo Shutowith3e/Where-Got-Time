12bac8a6eecf58008b07c126e2b7d6a6d3778b48
Joey <joey.chik05@gmail.com>
2025-07-10 13:38:43 +0800

    ci: lots of netlify issues for deploying both the fe and backend test 1

--- backend/functions/api.js
+++ backend/functions/api.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Router } from 'express';
 import cors from "cors";
 import env from "dotenv";
 import serverless from "serverless-http";
@@ -13,7 +13,7 @@ app.use(express.json());
 
 // this allows all origins, replace it before deployment
 app.use(cors());
-app.options("*",cors());
+app.options("*", cors());
 
 // replace it with this! 
 // app.use(cors({
@@ -30,23 +30,36 @@ import authJWT from '../middleware/auth.js';
 app.use(authJWT); // all routes after this needs a jwt in the auth header to work
 
 // import and use exported routes from groupRouter 
+const allRoutes = Router();
 import groupRoutes from '../routers/groupRouter.js';
 import eventRoutes from '../routers/eventRouter.js';
 import userRoutes from '../routers/userRouter.js';
 import adminRoutes from '../routers/adminRouter.js';
 
-app.use('/groups', groupRoutes);
-app.use('/events', eventRoutes);
-app.use('/users', userRoutes);
+allRoutes.use('/groups', groupRoutes);
+allRoutes.use('/events', eventRoutes);
+allRoutes.use('/users', userRoutes);
 
 //auth test route, delete later
 import authrouter from '../authtest.js';
-app.use('/test', authrouter);
+allRoutes.use('/test', authrouter);
 
 //import middleware for admin verification
 import mCheckAdmin from '../middleware/checkAdminMiddleware.js';
 
-app.use('/admins', mCheckAdmin, adminRoutes)
+allRoutes.use('/admins', mCheckAdmin, adminRoutes)
+app.use("/api", allRoutes)
 // check if server is running
 //app.listen(8000, () => console.log('Server running on port 8000'));
-export const handler = serverless(app);
+export const handler = serverless(app, {
+  request: (req, event, context) => {
+    if (event.body && typeof event.body === 'string') {
+      try {
+        req.body = JSON.parse(event.body);
+      } catch (e) {
+        // Leave it as is if it’s not JSON
+      }
+    }
+    return req;
+  }
+})
